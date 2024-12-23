b624f00ced927aa6390c8b9beb751878dbf2066f
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-23 22:49:00 +0200

    Add array info

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,13 @@
 package app;
 
+import java.util.Arrays;
+
 public class Main {
     static int[] array;
 
     public static void main(String[] args) {
+
+        array = new int[] {14, 56, 7, 9, 6, 81, 17, 36, 64, 73, 13, 66, 49, 28, 2};
+        System.out.println("\n1) Not sorted numbers of array:\n" + Arrays.toString(array));
     }
 }
