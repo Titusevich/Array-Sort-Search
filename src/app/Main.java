c36339caa5f934a9bce0c69cc4f16e59af55f3cc
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-23 22:53:49 +0200

    Add insertion sort of array

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,7 +7,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        array = new int[] {14, 56, 7, 9, 6, 81, 17, 36, 64, 73, 13, 66, 49, 28, 2};
-        System.out.println("\n1) Not sorted numbers of array:\n" + Arrays.toString(array));
+        array = new int[]{14, 56, 7, 9, 6, 81, 17, 36, 64, 73, 13, 66, 49, 28, 2};
+        System.out.println("\n1) Not sorted numbers of array:\n" +
+                Arrays.toString(array));
+
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        System.out.println("\n2) Sorted numbers of array:\n" +
+                Arrays.toString(array));
     }
 }
